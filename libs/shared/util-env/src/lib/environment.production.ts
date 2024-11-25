895ab014d55dc45c2c545ff4a852cb50a3e908fa
salimfenzar <s.fenzar@student.avans.nl>
2024-11-25 11:50:15 +0100

    static database

--- libs/shared/util-env/src/lib/environment.production.ts
+++ libs/shared/util-env/src/lib/environment.production.ts
@@ -5,5 +5,5 @@ export const environment: IEnvironment = {
 
   ROOT_DOMAIN_URL: 'https://my-app-api.azurewebsites.net', 
   dataApiUrl: 'https://my-app-api.azurewebsites.net/api',       
-  MONGO_DB_CONNECTION_STRING: '', 
+  MONGO_DB_CONNECTION_STRING: 'mongodb+srv://salimfenzar:Eastpak10@cluster0.whzhx.mongodb.net/recipe?retryWrites=true&w=majority'  
 };
