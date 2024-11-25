3940bac88c4ccc3bce62d34411a5d3938f5c7dc4
salimfenzar <s.fenzar@student.avans.nl>
2024-11-25 15:46:14 +0100

    changed

--- libs/shared/util-env/src/lib/environment.production.ts
+++ libs/shared/util-env/src/lib/environment.production.ts
@@ -3,7 +3,7 @@ import { IEnvironment } from './environment.interface';
 export const environment: IEnvironment = {
   production: true,
 
-  ROOT_DOMAIN_URL: 'https://my-app-api.azurewebsites.net', 
-  dataApiUrl: 'https://my-app-api.azurewebsites.net/api',       
+  ROOT_DOMAIN_URL: 'https://data-api-recipe-ajbbe7arg3ckh0du.westeurope-01.azurewebsites.net/', 
+  dataApiUrl: 'https://data-api-recipe-ajbbe7arg3ckh0du.westeurope-01.azurewebsites.net/api',       
   MONGO_DB_CONNECTION_STRING: 'mongodb+srv://salimfenzar:Eastpak10@cluster0.whzhx.mongodb.net/recipe?retryWrites=true&w=majority'  
 };
